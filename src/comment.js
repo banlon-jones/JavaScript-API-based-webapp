3ef818a7dc347bc2681fe674afef9b57c17a6ea8
banlonjones <banlonjones@go-groups.net>
2022-02-04 01:14:32 +0100

    comments

---
+++ src/comment.js
@@ -0,0 +1,36 @@
+export const modalSection = (movie, count) => `<div class="popup-container">
+                <div class="popup">
+                    <button class="btn close">X</button>
+                    <div class="details">
+                        <div>
+                            <img class="img" src="${movie.image.original}" height="450" width="500"/>
+                        </div>
+                        <div>
+                            <ul class="prop">
+                                <h1> ${movie.name} </h1>
+                                <li> Language: ${movie.language}</li>
+                                <li> Type: ${movie.type}</li>
+                                <li> Premiered: ${movie.premiered}</li>
+                                <li> Rating: ${movie.rating.average}</li>
+                                <li> Network: ${movie.network.name}</li>
+                                <li> URL: ${movie.url}</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="summary">
+                        <p> ${movie.summary} </p>
+                    </div>
+                    <div>
+                        <h2> Comments (${count}) </h2>
+                        <ul class="comments">
+                        </ul>
+                    </div>
+                    <div>
+                        <form id="comment-form" name="comment-form" class="details">
+                            <input type="text" name="name" placeholder="your name" required/>
+                            <input type="text" name="comment" placeholder="Leave a comment" required/>
+                            <button class="btn"> Post </button>
+                        </form>
+                    </div>
+                </div>
+            </div>`;
--- src/involvementAPI.js
+++ src/involvementAPI.js
@@ -20,3 +20,32 @@ export const getLikes = async () => {
     });
   return response.json();
 };
+
+export const getComments = async (movieID) => {
+  console.log(`${baseURL}apps/${appId}/comments?item_id=${movieID}`);
+  const response = await fetch(`${baseURL}apps/${appId}/comments?item_id=${movieID}`,{
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+  return response.json();
+};
+
+export const newComment = async (comment) => {
+  fetch(`${baseURL}apps/${appId}/comments`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(comment)
+  });
+};
+
+export const countComments = async (movieID) => {
+  const comments = await getComments(movieID);
+  if (comments.length !== undefined) {
+    return comments.length;
+  } else {
+    return 0;
+  }
+};
