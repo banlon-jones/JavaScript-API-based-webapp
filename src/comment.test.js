c8698a4e3240f9c59d944c9ea7d12488f6860261
banlonjones <banlonjones@go-groups.net>
2022-02-04 01:48:22 +0100

    comments test

---
+++ src/comment.test.js
@@ -0,0 +1,16 @@
+/** * @jest-environment jsdom */
+import {countComments} from './involvementAPI';
+
+global.fetch = jest.fn(() => Promise.resolve(
+  {
+    json: () => Promise.resolve(new Array(7)),
+  },
+));
+
+describe('Testing comment count', () => {
+  document.body.innerHTML = '<div></div>';
+  it('Comments count should be 7', async () => {
+    const count = await countComments();
+    expect(count).toBe(7);
+  });
+});
